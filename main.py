ce74bf087cf522233392799ccca322321bc2eac1
today <135026855+quthery@users.noreply.github.com>
2024-03-04 21:04:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import random
 import time
 import math
 
-# Размеры карты (в a.e.)
-map_width = 10  # Ширина карты
-map_height = 10  # Высота карты
 
-# Количество звезд
+map_width = 10 
+map_height = 10  
+
+
 num_stars = 3
 
-# Генерация случайных координат для звезд
+
 stars = [(random.randint(0, map_width - 1), random.randint(0, map_height - 1)) for _ in range(num_stars)]
 
-# Создание матрицы карты с звездами
+
 map_with_stars = [["." for _ in range(map_width)] for _ in range(map_height)]
 for star in stars:
     x, y = star
     map_with_stars[y][x] = "*"
 
-# Функция для вывода карты с звездами
+
 def print_map_with_stars(map_with_stars):
     for row in map_with_stars:
         print(" ".join(row))
@@ -31,7 +31,7 @@ def calculate_distances(user_x, user_y, stars):
         distances.append(distance)
     return distances
 
-# Цикл для исследования
+
 while True:
     print("\nNew exploration cycle started!")
     print_map_with_stars(map_with_stars)
@@ -43,21 +43,20 @@ while True:
     user_input_x = int(user_input_x)
     user_input_y = int(input("Enter Y coordinate: "))
 
-    # Вычисление расстояний до звезд
     distances = calculate_distances(user_input_x, user_input_y, stars)
 
-    # Проверка на нахождение звезды
+
     for i, distance in enumerate(distances):
         if distance == 0:
             print("You found a star!")
-            stars[i] = (-1, -1)  # Удаление звезды из списка
+            stars[i] = (-1, -1)  
 
-    # Обновление карты после нахождения звезды
+
     for i, star in enumerate(stars):
         if star == (-1, -1):
             map_with_stars[user_input_y][user_input_x] = "!"  # Обозначаем место найденной звезды
             print_map_with_stars(map_with_stars)
-            stars.pop(i)  # Удаляем звезду из списка
+            stars.pop(i)
 
     if not stars:
         print("All stars founded!")
@@ -65,7 +64,7 @@ while True:
         if finish_exploration.lower() == "y":
             break
 
-    # Вывод результата на английском
+
     print("\nDistances to stars:")
     for i, distance in enumerate(distances):
         if distance == 0:
